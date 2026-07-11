cc76ca8b565cd27422fe1b111e538cf1aef2b81c
Egg-03 <111327101+eggy03@users.noreply.github.com>
2026-07-11 13:51:36 +0530

    refactor: remove sdk - PART 4
    - Removed sdk components from reflection metadata

--- src/main/java/io/github/eggy03/papertrail/bot/entity/CachedMessage.java
+++ src/main/java/io/github/eggy03/papertrail/bot/entity/CachedMessage.java
@@ -1,10 +1,11 @@
 package io.github.eggy03.papertrail.bot.entity;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
 import lombok.NonNull;
 
+@RegisterForReflection
 public record CachedMessage(
         @NonNull String messageId,
         @NonNull String messageContent,
         @NonNull String authorId
-) {
-}
+) {}
